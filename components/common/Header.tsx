3d1e0b4a162fb215c0d673d0883d1afc5aef4e07
ritesh-sharma33 <sharmaritesh3312@gmail.com>
2020-09-15 22:40:36 +0530

    Changed the color of navbar and fixed the toggle collapse issue

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
@@ -13,16 +13,23 @@ const navLinkStyle = {
 };
 
 export default ({}) => {
+
+  let [isHeaderExpanded, setIsHeaderExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsHeaderExpanded(!isHeaderExpanded);
+  }
+
   return (
-    <div className="headerSection" style={{ backgroundColor: "#212121" }}>
-      <Navbar collapseOnSelect expand="sm">
+    <div className="headerSection" style={{ backgroundColor: "#212121", color: "white" }}>
+      <Navbar onToggle={handleToggle} expanded={isHeaderExpanded} variant="light" collapseOnSelect expand="sm">
         <Navbar.Brand href="#home"><img height="80" width="220" src="https://user-images.githubusercontent.com/45563022/89736314-2b96d700-da86-11ea-9fe9-06c416034218.gif" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle className="custom-toggler" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link className="navlink" style={navLinkStyle} as={NavLink} to='/' exact><span className="navlinkText">Home</span></Nav.Link>
-            <Nav.Link className="navlink" style={navLinkStyle} as={NavLink} to='/about'><span className="navlinkText">About</span></Nav.Link>
-            <Nav.Link className="navlink" style={navLinkStyle} as={NavLink} to='/blogs'><span className="navlinkText">Blogs</span></Nav.Link>
+            <Nav.Link onClick={handleToggle} className="navlink" style={navLinkStyle} as={NavLink} to='/' exact><span className="navlinkText">Home</span></Nav.Link>
+            <Nav.Link onClick={handleToggle} className="navlink" style={navLinkStyle} as={NavLink} to='/about'><span className="navlinkText">About</span></Nav.Link>
+            <Nav.Link onClick={handleToggle} className="navlink" style={navLinkStyle} as={NavLink} to='/blogs'><span className="navlinkText">Blogs</span></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
